a380df762caa21b12eed5b5af5ef51f50569f3ff
Xavier Davis <ulaughulose@gmail.com>
2026-07-13 16:58:30 -0400

    Create TodoList and TodoForm components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
+import TodoForm from './TodoForm.jsx'
+import TodoList from './TodoList.jsx'
 import './App.css'
 
 function App() {
-  const todoList = [
-    { id: 1, title: 'analyze assignments' },
-    { id: 2, title: 'put repo through CTD AI' },
-    { id: 3, title: 'final check' },
-  ]
-
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <TodoForm />
+      <TodoList />
     </div>
   )
 }
