dd662b76a495e92822b6f24e7644eb1656c87453
Xavier Davis <ulaughulose@gmail.com>
2026-07-28 05:30:40 -0400

    Add dynamic todo form

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,13 @@
-import TodoListItem from './TodoListItem.jsx'
-function TodoList({ todoList }) {
- 
+import TodoListItem from './TodoListItem.jsx';
 
+function TodoList({ todoList }) {
   return (
     <ul>
       {todoList.map((todo) => (
         <TodoListItem key={todo.id} todo={todo} />
       ))}
     </ul>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
