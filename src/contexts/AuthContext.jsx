9cb1a8dd2066e35c892f04cd5d7d64b96b5f4d90
Xavier Davis <ulaughulose@gmail.com>
2026-09-01 21:19:49 -0400

    use auth

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return context;
+}
